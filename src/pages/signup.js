cafd083dbaacde78147b3afebded7dd4f24ce5dc
Fivea5 <casul.alexis@yahoo.com>
2023-03-04 17:34:09 -0500

    Created a React based Signup page #38
    The design is not perfectly mirrored from the figma signup page, but will be in future edits

---
+++ src/pages/signup.js
@@ -0,0 +1,40 @@
+import './App.css';
+
+const SignUpPage = () => {
+  const TextBar = (props) => {
+    return(
+      <>
+        <div className="signup-div">
+          <div className="signup-text">
+            <p className="signup-title">{props.title}</p>
+          </div>
+          <div className="signup-boxes">
+            <input id="signup-text-box" type="text" name={props.title}/>
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  const SignupButton = () => {
+    return(
+      <>
+        <button className="signupButton">Sign Up</button>
+      </>
+    )
+  }
+
+  return (
+    <div className="App">
+      <h1 className="signupGreeting">Sign Up</h1>
+      <TextBar title={"Username"}/>
+      <TextBar title={"Email"}/>
+      <TextBar title={"Password"}/>
+      <TextBar title={"Confirm Password"}/>
+      <br></br>
+      <SignupButton />
+    </div>
+  );
+}
+
+export default SignUpPage;

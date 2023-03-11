2946fa029685ae24353838bcc67f94d6e6501bec
johntolo <johntolo@buffalo.edu>
2023-03-11 16:00:22 -0500

    Static pages for Edit Profile and Account Settings

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import Navbar from './components/Navbar';
 import FeedArea from './components/home/FeedArea';
 import SlideoutMenu from './components/SlideoutMenu';
+import Profile from './components/Profile/Profile.js';
 import React from 'react'
 
 import { useState } from 'react';
 
 function App() {
-  const [showSlideout, setShowSlideout] = useState(false)
-  
-  function displaySlideoutMenu(){
-    setShowSlideout(!showSlideout)
-  }
-  
   return (
-    <div className="App">
-      <Navbar displaySlideoutMenu={displaySlideoutMenu}/>
-      {showSlideout && <SlideoutMenu />}
-      <FeedArea />
-    </div>
-  );
+    <Profile />
+  )
 }
 
 export default App;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,9 @@
 import pfp from '../../images/pfp.jpg';
 import '../../style/profile.css';
 import React, {Component} from 'react'
+import { useState } from 'react';
 
-const ProfilePage = () => {
+const EditProfilePage = () => {
 const Name = () => {
     return (
         <div className='nameDiv'>
@@ -16,7 +17,7 @@ const Name = () => {
 const Username = () => {
     return (
         <div className='nameDiv'>
-            <p className='profile-text'>Username</p>
+            <p className='profile-text'>Display Name</p>
             <br></br>
             <input id = "profile-text-box"/>
         </div>
@@ -43,26 +44,20 @@ const Bio = () => {
     )
 }
 
-const Email = () => {
-    return (
-        <div className='nameDiv'>
-            <p className='profile-text'>Email</p>
-            <br></br>
-            <input id = "profile-text-box"/>
-        </div>
-    )
-}
-
     return (
         <div className='App' style={{textAlign:'center'}}>
             <div className='navigator'>
-            <div style = {{display: "flex", alignItems: "center"}}>
+            <div style = {{display: "flex"}}>
                 <div>
                     <div className = "hamburger"></div>
                     <div className = "hamburger"></div>
                     <div className = "hamburger"></div>
                 </div>
-            <p style = {{color: "black", fontFamily: "Times", fontSize: "35px", backgroundColor: "#FFE455"}}>Profile</p>
+            <p style = {{color: "black", fontFamily: "Times", fontSize: "35px", backgroundColor: "#FFE455", textAlign:'center'}}>Settings</p>
+            </div>
+            <div className='settings-button'>
+                <button style = {{backgroundColor: "#B3B3B3", fontFamily: "Times"}}>Edit Profile</button>
+                <button style = {{backgroundColor: "#FFE455", fontFamily: "Times"}}>Account Settings</button>
             </div>
             </div>
             <br></br>
@@ -70,7 +65,7 @@ const Email = () => {
             <img className="profile-pfp" src = {pfp}></img>
             <br></br>
             <br></br>
-            <button style = {{fontFamily: "Times", backgroundColor: "#FFE455"}}>Edit Profile Photo</button>
+            <button style = {{fontFamily: "Times", backgroundColor: "#FFE455", borderRadius:6}}>Edit Profile Photo</button>
             <br></br>
             <br></br>
             <br></br>
@@ -83,11 +78,98 @@ const Email = () => {
             <br></br>
             <Bio />
             <br></br>
-            <Email />
+            <br></br>
+            <button style = {{backgroundColor: '#00C52B', height:40, fontFamily: "Times", borderRadius:8}}>Save Changes</button>
             <br></br>
             <br></br>
         </div>
     )
 }
 
-export default ProfilePage
+const AccountSettingsPage = () => {
+    const Email = () => {
+        return (
+            <div className='nameDiv'>
+                <p className='profile-text'>Email</p>
+                <br></br>
+                <input id = "profile-text-box"/>
+            </div>
+        )
+    }
+
+    const Password = () => {
+        return (
+            <div className='nameDiv'>
+                <p className='profile-text'>Password</p>
+                <br></br>
+                <input id = "profile-text-box"/>
+            </div>
+        )
+    }
+
+    const PhoneNumber = () => {
+        return (
+            <div className='nameDiv'>
+                <p className='profile-text'>Phone Number</p>
+                <br></br>
+                <input id = "profile-text-box"/>
+            </div>
+        )
+    }
+
+    const FirstName = () => {
+        return (
+            <div className='nameDiv'>
+                <p className='profile-text'>First Name</p>
+                <br></br>
+                <input id = "profile-text-box"/>
+            </div>
+        )
+    }
+
+    const LastName = () => {
+        return (
+            <div className='nameDiv'>
+                <p className='profile-text'>Last Name</p>
+                <br></br>
+                <input id = "profile-text-box"/>
+            </div>
+        )
+    }
+
+    return (
+        <div className='App' style={{textAlign:'center'}}>
+            <div className='navigator'>
+            <div style = {{display: "flex"}}>
+                <div>
+                    <div className = "hamburger"></div>
+                    <div className = "hamburger"></div>
+                    <div className = "hamburger"></div>
+                </div>
+            <p style = {{color: "black", fontFamily: "Times", fontSize: "35px", backgroundColor: "#FFE455", textAlign:'center'}}>Settings</p>
+            </div>
+            <div className='settings-button'>
+                <button style = {{backgroundColor: "#FFE455", fontFamily: "Times"}}>Edit Profile</button>
+                <button style = {{backgroundColor: "#B3B3B3", fontFamily: "Times"}} onClick = {AccountSettingsPage}>Account Settings</button>
+            </div>
+            </div>
+            <br></br>
+            <FirstName/>
+            <br></br>
+            <LastName/>
+            <br></br>
+            <Email />
+            <br></br>
+            <PhoneNumber/>
+            <br></br>
+            <Password/>
+            <br></br>
+            <br></br>
+            <button style = {{height:30, width:300, fontFamily: "Times", borderRadius: 6}}>Change Password</button>
+            <br></br>
+            <br></br>
+            <button style = {{backgroundColor: '#00C52B', height:40, fontFamily: "Times", borderRadius:8}}>Save Changes</button>
+            </div>
+    )
+}
+export default EditProfilePage

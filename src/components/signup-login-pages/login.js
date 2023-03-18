36ae4eb11a3928cde9b7017d86564381cdd66f7f
YUBILAND <90258300+YUBILAND@users.noreply.github.com>
2023-03-18 02:39:49 -0400

    login and register work, need to expand database to accommodate email too

--- src/components/signup-login-pages/login.js
+++ src/components/signup-login-pages/login.js
@@ -1,57 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../../style/signupLogin.css';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
-  const TextBar = (props) => {
-    return(
-      <>
-        <div className="signup-div">
-          <div className="signup-text">
-            <p className="signup-title">{props.title}</p>
-          </div>
-          <div className="signup-boxes">
-            <input className="signupTextBox" id="signup-text-box" type="text" name={props.title}/>
-          </div>
-        </div>
-      </>
-    )
-  }
-
-  const Button = (props) => {
-    return(
-      <>
-        <button className="login-signup-button">{props.title}</button>
-      </>
-    )
-  }
   
+  const [details, setDetails] = useState({username: "", password: ""});
+  const[user, setUser] = useState({username: "", password: ""});
+  const[error, setError] = useState("");
+
+  let navigate = useNavigate();
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    
+
+    console.log(details);
+    setError("");
+
+    // Login(details);
+
+    axios.post('http://localhost/login.php', {
+      username: details.username,
+      password: details.password
+    }).then(val => {
+ 
+    if(val.data === "Wrong username or password"){
+      console.log("Wrong Username or Password");
+      setError("Wrong Username or Password");
+      
+    } else{
+      
+      console.log(val.data);
+      console.log("Logged in");
+      setUser({
+        username: details.username,
+        password: details.password
+      })
+      console.log(user.username);
+    }
+    
+  })
+
+}
+const isFirstRender = useRef(true);
+const isSecondRender = useRef(true)
+//two useRefs because react hooks double render when react strict mode is on
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return; // 👈️ return early if initial render
+    }
+    if (isSecondRender.current) {
+      isSecondRender.current = false;
+      return; // 👈️ return early if initial render
+    }
+
+    if (user.username !== ""){
+      navigate("/");
+    } else alert("invalid user"); console.log(user.username);
+
+  }, [user]); // 👈️ add state variables you want to track
 
   return (
     <div className="login-signup-app">
       <h1 className="signupGreeting">Log In</h1>
+      {(error !== "") ? (<div className="error">{error}</div> ): ""}
       <div className="login-form">
         <div className="login-text">
           <label htmlfor="username"><p className="login-title">Username</p></label>
         </div>
-        <input type="text" name="username" id="username"></input>
+        <input type="text" name="username" id="username" onChange={e => setDetails({...details, username: e.target.value})} value={details.username}></input>
       </div>
       <div className="login-form">
         <div className="login-text">
           <label htmlfor="password"><p className="login-title">Password</p></label>
         </div>
-        <input type="text" name="password" id="password"></input>
+        <input type="text" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}></input>
       </div>
       <p className="forgotPass">forgot your password?</p>
       <br></br>
-      <button className="login-signup-button">Log In</button>
+      <button className="login-signup-button" onClick = {loginHandler} >Log In</button>
       <p className="dontHaveAccount">Don't have an account?</p>
 
       <div className="signUpButton">
         <button className="login-signup-button">Sign Up</button>
       </div>
       
+    
+  
     </div>
   );
 }
 
 export default LoginPage;
+
--- src/components/signup-login-pages/signup.js
+++ src/components/signup-login-pages/signup.js
@@ -1,53 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../../style/signupLogin.css';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 const SignUpPage = () => {
-  const TextBar = (props) => {
-    return(
-      <>
-        <div className="signup-div">
-          <div className="signup-text">
-            <p className="signup-title">{props.title}</p>
-          </div>
-          <div className="signup-boxes">
-            <input id="signup-text-box" type="text" name={props.title}/>
-          </div>
-        </div>
-      </>
-    )
-  }
 
-  const SignupButton = () => {
-    return(
-      <>
-        <button className="signupButton">Sign Up</button>
-      </>
-    )
-  }
+  const [details, setDetails] = useState({username: "", email: "", password: ""});
+  const[regSuccess, setRegSuccess] = useState("");
+
+  let navigate = useNavigate();
+
+  const registerHandler = async (e) => {
+    e.preventDefault();
+    
+    console.log(details);
+
+    setRegSuccess("");
+    
+
+    axios.post('http://localhost/register.php', {
+        username : details.username,
+        password : details.password,
+        email : details.email
+        
+      }).then(val => {
+
+    if(val.data === "Username already taken"){
+      console.log("Username already taken");
+      setRegSuccess("Username already taken");
+      
+      
+    } else{
+      console.log(val.data);
+      console.log("Successfully Registered");
+      setRegSuccess("Successfully Registered");
+    }
+      
+  })
+
+}
 
   return (
     <div className="login-signup-app">
       <h1 className="signupGreeting">Sign Up</h1>
-
+      {(regSuccess !== "") ? (<div className="error">{regSuccess}</div> ): ""}
       <div className="signup-form">
         <div className="signup-text">
           <label htmlfor="username"><p className="login-title">Username</p></label>
         </div>
-        <input type="text" name="username" id="username"></input>
+        <input type="text" name="username" id="username" onChange={e => setDetails({...details, username: e.target.value})} value={details.username}></input>
       </div>
 
       <div className="signup-form">
         <div className="signup-text">
           <label htmlfor="email" ><p className="login-title">Email</p></label>
         </div>
-        <input type="text" name="email" id="email"></input>
+        <input type="text" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}></input>
       </div>
 
       <div className="signup-form">
         <div className="signup-text">
           <label htmlfor="password"><p className="login-title">Password</p></label>
         </div>
-        <input type="text" name="password" id="password"></input>
+        <input type="text" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}></input>
       </div>
 
       <div className="signup-form">
@@ -58,7 +73,7 @@ const SignUpPage = () => {
       </div>
 
       <br></br>
-      <SignupButton />
+      <button className="signupButton" onClick={registerHandler} >Sign Up</button>
     </div>
   );
 }

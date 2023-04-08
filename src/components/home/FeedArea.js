91b4c8f2b0c4edf3e571c421a39e3e8bb7b52f42
YUBILAND <90258300+YUBILAND@users.noreply.github.com>
2023-04-08 18:23:47 -0400

    swapped pfp and thumbnail #76

--- src/components/home/FeedArea.js
+++ src/components/home/FeedArea.js
@@ -52,35 +52,12 @@ const FeedArea = () => {
 
     {post.map((value, idx) => {
           return (
-            <FeedPost pfp = {`https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442b/uploads/${post[idx].thumbnail}`} posterName = {post[idx].poster} title = {post[idx].title} 
-        thumbnail= {dummy_post} numBookmarked = "400" eventTag= {post[idx].type} allowClickEvent={allowClickEvent}/>
+            <FeedPost pfp = {dummy_pfp} posterName = {post[idx].poster} title = {post[idx].title} 
+        thumbnail= {`https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442b/uploads/${post[idx].thumbnail}`} numBookmarked = "400" eventTag= {post[idx].type} allowClickEvent={allowClickEvent}/>
            )
           })}
 
-        {/* <FeedPost pfp = {dummy_pfp} posterName = "Company 1" title = "Firework showing at Fairgrounds!" 
-        thumbnail= {dummy_post} numBookmarked = "400" eventTag= "Recreation" allowClickEvent={allowClickEvent}/>
-
-        <FeedPost pfp = {dummy_pfp} posterName = "Company 2" title = "Basketball Tournament @ Ruckers!" 
-        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
-
-        <FeedPost pfp = {dummy_pfp} posterName = "Company 3" title = "Firework showing at Fairgrounds!" 
-        thumbnail= {dummy_post} numBookmarked = "400" eventTag= "Recreation" />
-
-        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
-        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
-
-        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
-        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
-        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
-        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
-        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
-        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
-        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
-        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
-        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
-        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
-        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
-        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/> */}
+       
     </div>
 
     <div>

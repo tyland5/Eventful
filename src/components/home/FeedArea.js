7f6d8c4c95dd0e94d66a6ccd8ba57e256af2c617
YUBILAND <90258300+YUBILAND@users.noreply.github.com>
2023-04-08 13:15:59 -0400

    events being displayed, no imgs yet, localhost #76

--- src/components/home/FeedArea.js
+++ src/components/home/FeedArea.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import FeedPost from './FeedPost'
 import dummy_pfp from "../../images/dummy-pfp.png"
 import dummy_post from "../../images/dummy-post.jpg"
@@ -18,12 +18,12 @@ const FeedArea = () => {
   }
 
 
-  const [post, setPost] = useState({"poster": "", "title": "", "type": "", "location": "", "description": "", "thumbnail": "", "images": ""});
+  // const [post, setPost] = useState([{poster: "", title: "", type: "", location: "", description: "", thumbnail: "", images: ""}]);
+  const [post, setPost] = useState([]);
   const[error, setError] = useState("");
 
-  const LoadPost = async (e) => {
-      e.preventDefault();
-    
+  
+    let arr = [];
       // axios.get("https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442b/load-event.php")
       axios.get("http://localhost/load-event.php")
       .then(val => {
@@ -31,15 +31,25 @@ const FeedArea = () => {
       if (val.data === "Nothing"){
         // No images
       } else {
-          console.log(val.data)
-
+          //console.log(val.data)
+          // for (const i in val.data){
+          //     //console.log("HELLO")
+          //     //console.log(val.data[i].poster)
+          //     setPost(post => [...post, {poster: val.data[i].poster, title: val.data[i].title,
+          //        type: val.data[i].type, location: val.data[i].location, description: val.data[i].description }])
+                 
+          // }
+          setPost(val.data)
+          
       }
     })
-
-
-  }
-
-
+    useEffect(() => {
+      if (post.length !== 0){
+          //console.log(post)
+      }
+  }, [post]);
+    
+    
 
 
 
@@ -49,9 +59,17 @@ const FeedArea = () => {
     
     
     <>
-    <p className="filter-btn">Filter</p>
-    <div className ="feed-area" onLoad={LoadPost}>
-        <FeedPost pfp = {dummy_pfp} posterName = "Company 1" title = "Firework showing at Fairgrounds!" 
+    <p className="filter-btn" >Filter</p>
+    <div className ="feed-area">
+
+    {post.map((value, idx) => {
+          return (
+            <FeedPost pfp = {dummy_pfp} posterName = {post[idx].poster} title = {post[idx].title} 
+        thumbnail= {dummy_post} numBookmarked = "400" eventTag= {post[idx].type} allowClickEvent={allowClickEvent}/>
+           )
+          })}
+
+        {/* <FeedPost pfp = {dummy_pfp} posterName = "Company 1" title = "Firework showing at Fairgrounds!" 
         thumbnail= {dummy_post} numBookmarked = "400" eventTag= "Recreation" allowClickEvent={allowClickEvent}/>
 
         <FeedPost pfp = {dummy_pfp} posterName = "Company 2" title = "Basketball Tournament @ Ruckers!" 
@@ -74,7 +92,7 @@ const FeedArea = () => {
         <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
         thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
         <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
-        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
+        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/> */}
     </div>
 
     <div>

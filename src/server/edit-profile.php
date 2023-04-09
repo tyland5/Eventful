1f7530479166c6a2c6cdc5e05154e405ce539a68
johntolo <johntolo@buffalo.edu>
2023-04-09 19:36:55 -0400

    finalLoadSettings #74

--- src/server/edit-profile.php
+++ src/server/edit-profile.php
@@ -12,31 +12,22 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 
 $conn = mysqli_connect("oceanus.cse.buffalo.edu", "dchen83", "50360060", "cse442_2023_spring_team_b_db", "3306");
-if($conn -> connect_error){
-    die("connection failed\n");
-}
-else{
-    print("Connected fine\n");
-}
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 	header('HTTP/1.1 200 OK');
 	exit();
 }
 
-$sess_id = "v51fllvag31alj1u0gp5e7vevn";
-$sess_id2 = "k08r5tt8rh38ooh0ortg9ogsfe";
-$sess_id3 = "f9lb341f559gg3d5745m1r3gvh";
-$sql = "SELECT * FROM `Sessions` WHERE `session_id` = '$sess_id2'";
+$sql = "SELECT * FROM `Sessions` ORDER BY `expiration` DESC LIMIT 1";
 $res = $conn->query($sql);
 $row = mysqli_fetch_row($res);
 $user_id = $row[0];
 
-
 //Upon receiving a POST request from axios
 if (isset($_POST)) {
+
     $data = json_decode(file_get_contents('php://input'), true);
-    
+
     $name = $data['name'];
     $displayname = $data['displayname'];
     $website = $data['website'];
@@ -55,4 +46,5 @@ if (isset($_POST)) {
 	    $res = $conn->query($sql);
     }
 }
+
 ?>

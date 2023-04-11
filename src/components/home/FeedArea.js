7a02b4b0fd95e4d75af44dab3d1e047d91e5678d
tyland5 <tyland5@users.noreply.github.com>
2023-04-11 18:15:46 -0400

    Create event filter menu #85
    Worked on the front end of things. Clicking on "Filter" brings up checkboxes. Checking a box means that you want to see those types of events. Not selecting anything will mean all types of events are allowed to be seen in feed.
    
    Clicking the filter posts button in the future should cause the updatePage variable to change, triggering the createEffect (and thus axios) which will repopulate the page with the filtered events

--- src/components/home/FeedArea.js
+++ src/components/home/FeedArea.js
@@ -8,6 +8,10 @@ import axios from 'axios'
 
 const FeedArea = () => {
   const[allowClickEvent, setClickEvent] = useState(true)
+  const[showFilters, setShowFilters] = useState(false)
+  const[filter, setFilter] = useState(eventTags) //upon first render of page, want to show all events
+  const[updatePage, setUpdatePage] = useState(true)
+  const eventTags = ["Recreation", "Volunteer", "Entertainment", "Food", "Adult"]
 
   function allowClickableEvent(){
     setClickEvent(!allowClickEvent)
@@ -17,37 +21,77 @@ const FeedArea = () => {
     return allowClickEvent
   }
 
+  function alterFilter(e){
+    const tag = e.target.value
+    
+    //constant time so not bad
+    if(filter.has(tag)){
+      filter.delete(tag)
+    }
+    else{
+      filter.add(tag)
+    }
+    console.log(filter)
+  }
+
+  function applyFilter(){
+    //Just in case people want to be weird. no filter applied
+    if(filter.size ===0){
+      setFilter(new Set())
+      filter.add("all")
+    }
+
+    console.log(filter)
+    setShowFilters(!showFilters)
+    
+  }
+
 
   // const [post, setPost] = useState([{poster: "", title: "", type: "", location: "", description: "", thumbnail: "", images: ""}]);
   const [post, setPost] = useState([]);
   const[error, setError] = useState("");
 
   
-    
-      axios.get("https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442b/load-event.php")
+    useEffect(() => {
+      
+      axios.post("https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442b/load-event.php", filter)
       // axios.get("http://localhost/load-event.php")
       .then(val => {
 
-      if (val.data === "Nothing"){
-        // No images
-      } else {
-         
+        if(val.data !== "Nothing"){
           setPost(val.data)
-          
-      }
-    })
-    useEffect(() => {
+        }
+      })
+      
       if (post.length !== 0){
           //console.log(post)
       }
-  }, [post]);
+    }, [updatePage]);
     
 
   return (
-    
-    
     <>
-    <p className="filter-btn" >Filter</p>
+    {/*<p className="filter-btn" >Filter</p>*/}
+
+    <p className='filter-btn' onClick={() => {setShowFilters(!showFilters); setFilter(new Set())}}>Filters</p>
+    {showFilters && eventTags.map(event => {
+      return(
+      <div className='filter-choice'>
+      <input className = "filter-box" type= "checkbox" id={event} value={event} onChange={e=>alterFilter(e)}/>
+      <label className = "filter-label" htmlFor= {event}>{event}</label> 
+      </div>
+      )})}
+    {showFilters && <button id = 'filter-submit-btn' onClick={applyFilter}>Filter Posts</button>}
+    {/*
+    <input type= "checkbox" id="Recreation" value="Recreation"/>
+    <label htmlFor="Recreation">Recreation</label> 
+    <input type= "checkbox" id="Recreation" value="Recreation"/>
+    <label htmlFor="Recreation">Recreation</label> 
+    <input type= "checkbox" id="Recreation" value="Recreation"/>
+    <label htmlFor="Recreation">Recreation</label> 
+    
+  */} 
+
     <div className ="feed-area">
       <PostButton/>
     {post.map((value, idx) => {
@@ -56,10 +100,7 @@ const FeedArea = () => {
         thumbnail= {`https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442b/uploads/${post[idx].thumbnail}`} numBookmarked = "400" eventTag= {post[idx].type} allowClickEvent={allowClickEvent}/>
            )
           })}
-
-       
     </div>
-
     </>
   )
 }

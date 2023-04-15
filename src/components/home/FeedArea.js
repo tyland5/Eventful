29a72c4eba492c85d9bb7ce6c5e533844c78d752
johntolo <johntolo@buffalo.edu>
2023-04-15 18:58:04 -0400

    StatefulNavbar + noSignUpButton

--- src/components/home/FeedArea.js
+++ src/components/home/FeedArea.js
@@ -1,63 +1,39 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import FeedPost from './FeedPost'
 import dummy_pfp from "../../images/dummy-pfp.png"
 import dummy_post from "../../images/dummy-post.jpg"
 import rutgers_park from "../../images/rutgers-park.jpg"
 import PostButton from '../createEvent/PostButton'
-import axios from 'axios'
 
 const FeedArea = () => {
-  const[allowClickEvent, setClickEvent] = useState(true)
-
-  function allowClickableEvent(){
-    setClickEvent(!allowClickEvent)
-  }
-
-  function giveClickEvent(){
-    return allowClickEvent
-  }
-
-
-  // const [post, setPost] = useState([{poster: "", title: "", type: "", location: "", description: "", thumbnail: "", images: ""}]);
-  const [post, setPost] = useState([]);
-  const[error, setError] = useState("");
-
-  
-    
-      axios.get("https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442b/load-event.php")
-      // axios.get("http://localhost/load-event.php")
-      .then(val => {
-
-      if (val.data === "Nothing"){
-        // No images
-      } else {
-         
-          setPost(val.data)
-          
-      }
-    })
-    useEffect(() => {
-      if (post.length !== 0){
-          //console.log(post)
-      }
-  }, [post]);
-    
-
   return (
-    
-    
     <>
-    <p className="filter-btn" >Filter</p>
+    <p className="filter-btn">Filter</p>
     <div className ="feed-area">
+        <FeedPost pfp = {dummy_pfp} posterName = "Company 1" title = "Firework showing at Fairgrounds!" 
+        thumbnail= {dummy_post} numBookmarked = "400" eventTag= "Recreation"/>
 
-    {post.map((value, idx) => {
-          return (
-            <FeedPost pfp = {dummy_pfp} posterName = {post[idx].poster} title = {post[idx].title} 
-        thumbnail= {`https://www-student.cse.buffalo.edu/CSE442-542/2023-Spring/cse-442b/uploads/${post[idx].thumbnail}`} numBookmarked = "400" eventTag= {post[idx].type} allowClickEvent={allowClickEvent}/>
-           )
-          })}
+        <FeedPost pfp = {dummy_pfp} posterName = "Company 2" title = "Basketball Tournament @ Ruckers!" 
+        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
 
-       
+        <FeedPost pfp = {dummy_pfp} posterName = "Company 3" title = "Firework showing at Fairgrounds!" 
+        thumbnail= {dummy_post} numBookmarked = "400" eventTag= "Recreation" />
+
+        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
+        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
+
+        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
+        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
+        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
+        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
+        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
+        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
+        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
+        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
+        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
+        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
+        <FeedPost pfp = {dummy_pfp} posterName = "Company 4" title = "Basketball Tournament @ Ruckers!" 
+        thumbnail= {rutgers_park} numBookmarked = "400" eventTag= "Recreation"/>
     </div>
 
     <div>
